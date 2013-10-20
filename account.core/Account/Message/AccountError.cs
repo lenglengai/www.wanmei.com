e66a4ba00c020d8ed8056d72199935a525a95dd4
zyouhua <zyouhua@gmail.com>
2013-10-20 21:29:37 +0800

    update

---
+++ account.core/Account/Message/AccountError.cs
@@ -0,0 +1,15 @@
+﻿namespace account.core
+{
+    public enum AccountError : uint
+    {
+        mSucess_,
+        mFail_,
+        mPassward_,
+        mDevice_,
+        mDeviceType_,
+        mDeviceId_,
+        mNoLogin_,
+        mServerId_,
+        mNoAccount_,
+    }
+}
