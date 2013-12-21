664f8edc8ca4bb11ae50324ea088b922951a2dcf
lenglengai <lenglengai@gmail.com>
2013-12-21 19:11:56 +0800

    update

--- platform/State/State.cs
+++ platform/State/State.cs
@@ -7,5 +7,22 @@ namespace platform
 {
     public class State
     {
+        public State(int nNo, int nSize) {
+            mMaxNo = nNo * nSize;
+            mNo = nNo;
+            int length = (int)Math.Log((nNo + 1), 2);
+            length *= nSize;
+            if ((length % 8) > 0){
+                length /= 8;
+                length += 1;
+            } else {
+                length /= 8;
+            }
+            mValue = new char[length];
+        }
+
+        char[] mValue;
+        int mMaxNo;
+        int mNo;
     }
 }
