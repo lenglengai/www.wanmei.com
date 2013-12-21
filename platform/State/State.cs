02a0f2bda638ffb4f68623c4ec4da1ee94b8a9a9
lenglengai <lenglengai@gmail.com>
2013-12-21 20:03:52 +0800

    update

--- platform/State/State.cs
+++ platform/State/State.cs
@@ -7,12 +7,28 @@ namespace platform
 {
     public class State
     {
+        public int _getIndex(out int nNo, int nIndex) {
+
+        }
+
+        public bool _setIndex(int nNo, int nIndex) {
+
+        }
+
+        public bool _setLeft(int nLeft) {
+            if (nLeft > mLeft) return false;
+
+        }
+
         public State(int nNo, int nSize) {
             mMaxNo = nNo * nSize;
             mNo = nNo;
             int length = (int)Math.Log((nNo + 1), 2);
             length *= nSize;
-            if ((length % 8) > 0){
+            mLeft = length % 8;
+            if (mLeft > 0) {
+                mLeft = 8 - mLeft;
+                mLeft = (int)Math.Pow(2, mLeft);
                 length /= 8;
                 length += 1;
             } else {
@@ -23,6 +39,7 @@ namespace platform
 
         char[] mValue;
         int mMaxNo;
+        int mLeft;
         int mNo;
     }
 }
